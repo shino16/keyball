05a9dd26205ed9b7fb77838de77eeb1ebac14741
Masato Shinokawa <shino16@users.noreply.github.com>
2025-11-02 15:55:47 +0900

    Enable "Hold On Other Key Press"

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/via/config.h
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/via/config.h
@@ -37,5 +37,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define DYNAMIC_KEYMAP_LAYER_COUNT 5
 
+#define HOLD_ON_OTHER_KEY_PRESS
+
 #define POINTING_DEVICE_AUTO_MOUSE_ENABLE
 #define AUTO_MOUSE_DEFAULT_LAYER 1
